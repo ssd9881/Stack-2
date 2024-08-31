0be894d28d84b24588758c76aa59e097d8d5d61b
sdumbre1 <sdumbre@charlotte.edu>
2024-08-31 10:09:32 -0400

    Completed Stack-2

---
+++ exclusive.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[] exclusiveTime(int n, List<String> logs) {
+        int []result = new int[n];
+        Stack<Integer> st = new Stack<>();
+        int curr =0;
+        int prev = 0;
+        for(String log: logs){
+            String [] strArr = log.split(":");
+            int currTask = Integer.parseInt(strArr[0]);
+            curr = Integer.parseInt(strArr[2]);
+            if(strArr[1].equals("start")){
+                if(!st.isEmpty()){
+                    result[st.peek()] += curr- prev;
+                }
+                st.push(currTask);
+                prev = curr;
+            }else{
+                result[st.pop()] += curr+1 - prev;
+                 prev = curr+1;
+            }
+        }
+        return result;
+    }
+}
---
+++ valid.java
@@ -0,0 +1,22 @@
+// TC:O(n), sc:O(1)
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+        for(int i=0;i<s.length();i++){
+            char c = s.charAt(i);
+            if(c=='['){
+                st.push(']');
+            }else if(c=='{'){
+                st.push('}');
+            }else if(c=='('){
+                st.push(')');
+            }else{
+                if(st.isEmpty() || st.pop()!=c){
+                    return false;
+                }
+            }
+        }
+        if(!st.isEmpty()) return false;
+        return true;
+    }
+}
